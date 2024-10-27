456731cc5b01629c2bd721bf5ce3f4e0b6e0adfe
b166erobot <bernard0freitas@outlook.com>
2024-10-27 12:22:54 -0300

    Requisitos 1 ao 4 satisfeitos

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,15 +1,36 @@
-from typing import List, Dict
+"""Module to process data from csv file."""
+
+import csv
+from typing import NoReturn
+from typing import List, Dict, TypeAlias
+
+JobsType: TypeAlias = Dict[str, int | str]
 
 
 class ProcessJobs:
-    def __init__(self) -> None:
-        self.jobs_list = list()
+    """Read and process csv jobs data."""
 
-    def read(self) -> List[Dict]:
-        pass
+    def __init__(self) -> None:
+        self.jobs_list = []
+
+    def read(self, file_name: str) -> NoReturn:
+        """Read a csv file and store dicts."""
+        with open(file_name, "r", encoding="utf8") as file:
+            reader = csv.reader(file)
+            header = next(reader)
+            self.jobs_list.extend((dict(zip(header, job)) for job in reader))
 
     def get_unique_job_types(self) -> List[str]:
-        pass
+        """Return unique job types."""
+        return list(set([job["job_type"] for job in self.jobs_list]))
 
-    def filter_by_multiple_criteria(self) -> List[dict]:
-        pass
+    def filter_by_multiple_criteria(
+        self, jobs: JobsType, criteria: JobsType
+    ) -> List[Dict]:
+        """Filter jobs by criteria."""
+        unique_jobs = {
+            frozenset(job.items())
+            for job in jobs
+            if all(job.get(key) == criteria[key] for key in criteria)
+        }
+        return list(map(dict, unique_jobs))
