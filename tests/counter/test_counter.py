705dfe6664bd07744a9953426512d42098fe7852
no_name <no@email.com>
2024-10-27 12:31:07 -0300

    erro de linter corrigido

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,10 +2,9 @@ from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    result_python = count_ocurrences('data/jobs.csv', 'Python')
+    result_python = count_ocurrences("data/jobs.csv", "Python")
     expected_python = 1639
-    result_javascript = count_ocurrences('data/jobs.csv', 'Javascript')
+    result_javascript = count_ocurrences("data/jobs.csv", "Javascript")
     expected_javascript = 122
     assert result_python == expected_python
     assert result_javascript == expected_javascript
-
