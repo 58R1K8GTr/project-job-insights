2e164d96eaa82351a024b873c17e331ccd01a35e
b166erobot <bernard0freitas@outlook.com>
2024-10-27 13:56:16 -0300

    Requisito 6 satisfeito

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -1,13 +1,20 @@
+"""Module to process csv data file."""
+
 from typing import Union, List, Dict
 from src.insights.jobs import ProcessJobs
 
 
 class ProcessSalaries(ProcessJobs):
-    def __init__(self):
-        super().__init__()
+    """Read and process csv jobs data."""
 
     def get_max_salary(self) -> int:
-        pass
+        """Returns max salary."""
+        max_salary = 0
+        for job in self.jobs_list:
+            str_salary = job["max_salary"]
+            salary = int(str_salary) if str_salary.isnumeric() else 0
+            max_salary = salary if salary > max_salary else max_salary
+        return max_salary
 
     def get_min_salary(self) -> int:
         pass
