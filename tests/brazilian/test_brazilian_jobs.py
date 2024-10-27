4546a5ed547fd7f993a73e207349aebdc33a1fe9
no_name <no@email.com>
2024-10-27 17:29:58 -0300

    Requisito 10 satisfeito

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,7 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    raise
+    result = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    expect = {"title", "salary", "type"}
+    assert all(set(job.keys()) == expect for job in result) is True
